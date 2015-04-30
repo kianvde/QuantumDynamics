242b041adaf59300820b060fb0a618e077eeb96d
kianvde <Kianvde@gmail.com>
2015-04-30 17:23:11 +0200

    Animation

--- 2DCrank.py
+++ 2DCrank.py
@@ -3,7 +3,7 @@ import scipy.sparse.linalg as linalg
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
-
+from mpl_toolkits.mplot3d import Axes3D
 n = 4
 N = n**2
 dt = 2.0
@@ -17,11 +17,22 @@ B = (1.-4.*s)*sparse.identity(N) + s*sparse.eye(N,N,1) + s*sparse.eye(N,N,-1) + 
 
 psi = np.zeros(N)
 psi[round(N/2 + n/2)] = 1.0
-
-[psi, garbage] = linalg.bicgstab(A, B*psi)
-psi = psi.reshape(n,n)
-
-plt.figure()
-plt.pcolor(abs(psi)**2)
-plt.colorbar()
+fig2 = plt.figure()
+ax = fig2.add_subplot(111,projection='3d')
+ims = []
+x=np.linspace(0,1,n)
+y=x
+for i in range(100):
+    [psi, garbage] = linalg.bicgstab(A, B*psi)
+    psi2 = psi.reshape(n,n)
+    im = ax.plot_surface(x,y,np.abs(psi2)**2),
+    ims.append(im)
+im_ani = animation.ArtistAnimation(fig2, ims, interval=1000, repeat_delay=3000, blit=True)
 plt.show()
+
+#####
+# [psi, garbage] = linalg.bicgstab(A, B*psi)
+# psi = psi.reshape(n,n)
+#
+# plt.figure()
+# plt.pcolor(abs(psi)**2)

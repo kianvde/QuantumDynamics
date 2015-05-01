4a1971f51ea98195b48d3b3f318ef1c0ea092c83
Marius van Eck <marius.v.eck@gmail.com>
2015-05-01 21:52:50 +0200

    File cleanup and commenting 1D Crank-Nicolson

--- QD_Crank_Nicolson.py
+++ QD_Crank_Nicolson.py
@@ -4,57 +4,62 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
 
-dt = 2.0
-n = 100         # number of points
-L = 10.0
-dx = L/n
+# simulation constants
+dt = 0.025
+dx = 0.01
+L = 60.0
+k = 5.
+n = np.floor(L/dx)
+
+# two constants following from two simplify the expressions
 c1 = 1.j * dt / (4. * dx**2)    # i*hbar*dt/(4*m*dx^2)
 c2 = 1.j * dt / 2.              # i*dt/(2*hbar)
-k = 5.
-bound = 0
+
+# generate x-coordinates and initial wave-function
+x = np.linspace(0,L,n)
+psi = np.exp(-0.1*(x-L/4)**2) * np.exp(1.j*k*x)
+psi /= np.sqrt(sum(abs(psi)**2*dx)) # normalize the wave function
 
 # set a potential
 v = 1.0*np.zeros(n)
+v[2972:3028] = 12.5
+# this barrier of width a = 56*dx = 0.56 should give a transmission of ~1/3
+# E = V0 (=(k^2)/2=12.5) yields: T = 1/(1+0.5*V*a^2)
 
-## numerically solve for the matrix equation A*psi(t+dt) = B*psi(t) where A and B are tridiagonal matrices ##
-
+# numerically solve for the matrix equation A*psi(t+dt) = B*psi(t) where A and B are tridiagonal
 A = sparse.diags(1.+2.*c1+c2*v, 0) - c1*sparse.eye(n,n,1) - c1*sparse.eye(n,n,-1)
 B = sparse.diags(1.-2.*c1-c2*v, 0) + c1*sparse.eye(n,n,1) + c1*sparse.eye(n,n,-1)
-if bound == 1:
-    A[0,0:] = 0.
-    A[n-1,0:] = 0.
-    A[0,0] = 1.
-    A[n-1,n-1] = 1.
-    B[0,0:] = 0.
-    B[n-1,0:] = 0.
-    B[0,0] = 1.
-    B[n-1,n-1] = 1.
-else:
-    A[0,n-2] = -c1
-    A[n-1,1] = -c1
-    B[0,n-2] = c1
-    B[n-1,1] = c1
+# fill in for periodic boundary conditions
+A = A + sparse.diags([[-c1, 0],[0, -c1]],[n-2, 2-n])
+B = B + sparse.diags([[c1, 0],[0, c1]],[n-2, 2-n])
 
 
-# generate x-coordinates and initial wave-function
-mean = L/2
-sigma = 1.0
-x = np.linspace(0,L,n)
-psi = np.exp(-(x-mean)**2/(2*sigma**2)) * np.exp(1.j*k*x)    # phase factor guessed
-psi /= np.sqrt(sum(abs(psi)**2*dx)) # normalize the wave function
-# psi[0] = 0.
-# psi[n-1] = 0.
-
-# start loop
 fig2, ax = plt.subplots()
+plt.vlines([L*(0.5-28/3000), L*(0.5+28/3000)], 0, 1)
 ims = []
-for i in range(100):
+for i in range(200):
+
+    # use the sparse stabilized biconjugate gradient method to solve the matrix eq
     [psi, garbage] = linalg.bicgstab(A, B*psi)
-    im, = plt.plot(x, np.abs(psi)**2, 'b')
-    ims.append([im])
-    print psi[0] , psi[-1]
+
+    if i%5 == 0:
+        im, = plt.plot(x[2000:4000], np.abs(psi[2000:4000])**2, 'b')
+        ims.append([im])
+
+print "norm at the end: ",
+print sum(abs(psi)**2)*dx
+print "norm left: ",
+print sum(abs(psi[0:2999])**2)*dx
+print "norm right: ",
+print sum(abs(psi[3000:])**2)*dx
+print "approximation Transmission: ",
+print sum(abs(psi[3000:])**2)*dx/(sum(abs(psi)**2)*dx)
+
+# animate after enter #
+try:
+    input("Press enter to continue")
+except SyntaxError:
+    pass
 im_ani = animation.ArtistAnimation(fig2, ims, interval=100, repeat_delay=3000, blit=True)
 plt.show()
-#####
-print "norm at the end: "
-print sum(abs(psi)**2)*dx
+

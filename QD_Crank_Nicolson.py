b70b2f2dd4791de37cee2c5c0ad96c7596edc725
Marius van Eck <marius.v.eck@gmail.com>
2015-05-07 17:25:59 +0200

    1D CN added plot of the transmission versus wave packet energy

--- QD_Crank_Nicolson.py
+++ QD_Crank_Nicolson.py
@@ -5,58 +5,78 @@ import numpy as np
 import matplotlib.animation as animation
 
 # simulation constants
-dt = 0.025
+dt = 0.05
 dx = 0.01
-L = 60.0
-k = 5.
+L = 100.0
+v0 = 6.125
+E = np.linspace(0.8,1.4,10)*v0
+a = 2.
+
+
+k_vector = np.sqrt(2*E)
+transmission = np.zeros(len(k_vector))
 n = np.floor(L/dx)
+for i,k in enumerate(k_vector):
 
-# two constants to simplify the expressions
-c1 = 1.j * dt / (4. * dx**2)    # i*hbar*dt/(4*m*dx^2)
-c2 = 1.j * dt / 2.              # i*dt/(2*hbar)
+    # two constants to simplify the expressions
+    c1 = 1.j * dt / (4. * dx**2)    # i*hbar*dt/(4*m*dx^2)
+    c2 = 1.j * dt / 2.              # i*dt/(2*hbar)
 
-# generate x-coordinates and initial wave-function
-x = np.linspace(0,L,n)
-psi = np.exp(-0.1*(x-L/4)**2) * np.exp(1.j*k*x)
-psi /= np.sqrt(sum(abs(psi)**2*dx))  # normalize the wave function
+    # generate x-coordinates and initial wave-function
+    x = np.linspace(0,L,n)
+    psi = np.exp(-0.005*(x-.25*L)**2) * np.exp(1.j*k*x)
+    psi /= np.sqrt(sum(abs(psi)**2*dx))  # normalize the wave function
 
-# set a potential
-v = 1.0*np.zeros(n)
-v[2972:3028] = 12.5
-# this barrier of width a = 56*dx = 0.56 should give a transmission of ~1/3
-# E = V0 (=(k^2)/2=12.5) yields: T = 1/(1+0.5*V*a^2)
+    # set a potential
+    v = np.zeros(n)
+    v[int(round((L - a)/(2*dx))):int(round((L + a)/(2*dx)))] = v0
+    # this barrier of width a = 56*dx = 0.56 should give a transmission of ~1/3
+    # E = V0 (=(k^2)/2=12.5) yields: T = 1/(1+0.5*V*a^2)
 
-# numerically solve for the matrix equation A*psi(t+dt) = B*psi(t) where A and B are tridiagonal
-A = sparse.diags(1.+2.*c1+c2*v, 0) - c1*sparse.eye(n,n,1) - c1*sparse.eye(n,n,-1)
-B = sparse.diags(1.-2.*c1-c2*v, 0) + c1*sparse.eye(n,n,1) + c1*sparse.eye(n,n,-1)
-# fill in for periodic boundary conditions
-A = A + sparse.diags([[-c1, 0],[0, -c1]],[n-2, 2-n])
-B = B + sparse.diags([[c1, 0],[0, c1]],[n-2, 2-n])
+    # numerically solve for the matrix equation A*psi(t+dt) = B*psi(t) where A and B are tridiagonal
+    A = sparse.diags(1.+2.*c1+c2*v, 0) - c1*sparse.eye(n,n,1) - c1*sparse.eye(n,n,-1)
+    B = sparse.diags(1.-2.*c1-c2*v, 0) + c1*sparse.eye(n,n,1) + c1*sparse.eye(n,n,-1)
+    # fill in for periodic boundary conditions
+    A = A + sparse.diags([[-c1, 0],[0, -c1]],[n-2, 2-n])
+    B = B + sparse.diags([[c1, 0],[0, c1]],[n-2, 2-n])
 
+    print "----calculating:",
+    print "E/V0 =",
+    print E[i]/v0,
+    print "----"
 
-fig2, ax = plt.subplots()
-plt.vlines([L*(0.5-28/3000), L*(0.5+28/3000)], 0, 1)
-ims = []
-for i in range(200):
+    fig, ax = plt.subplots()
+    plt.vlines([(L-a)/2, (L+a)/2], 0, 2*max(abs(psi)**2))
+    ims = []
 
-    if i%5 == 0:
-        im, = plt.plot(x[2000:4000], np.abs(psi[2000:4000])**2, 'b')
-        ims.append([im])
+    c = 0
+    c_old = 0
+    j = 0
+    while c_old<=c or c>10e-4:
+        c_old = c
+        if j%10 == 0:
+            im, = plt.plot(x, np.abs(psi)**2, 'b')
+            ims.append([im])
 
-    # use the sparse stabilized biconjugate gradient method to solve the matrix eq
-    [psi, garbage] = linalg.bicgstab(A, B*psi)
+        # use the sparse stabilized biconjugate gradient method to solve the matrix eq
+        [psi, garbage] = linalg.bicgstab(A, B*psi, x0=psi)
 
-print "norm at the end: ",
-print sum(abs(psi)**2)*dx
-print "norm left: ",
-print sum(abs(psi[0:2999])**2)*dx
-print "norm right: ",
-print sum(abs(psi[3000:])**2)*dx
-print "approximation Transmission: ",
-print sum(abs(psi[3000:])**2)*dx/(sum(abs(psi)**2)*dx)
+        j += 1
+        c = sum(abs(psi[int(round((L - a)/(2*dx))):int(round((L + a)/(2*dx)))])**2)*dx
 
-try: input("continue to animation")
-except SyntaxError: pass
-im_ani = animation.ArtistAnimation(fig2, ims, interval=100, repeat_delay=3000, blit=True)
+    print "norm at the end: ",
+    print sum(abs(psi)**2)*dx
+    print "norm left: ",
+    print sum(abs(psi[0:int(round((L - a)/(2*dx)))])**2)*dx
+    print "norm right: ",
+    print sum(abs(psi[int(round((L - a)/(2*dx))):])**2)*dx
+    transmission[i] = sum(abs(psi[int(round((L - a)/(2*dx))):])**2)*dx/(sum(abs(psi)**2)*dx)
+    print "approximation Transmission: ",
+    print transmission[i]
+
+    im_ani = animation.ArtistAnimation(fig, ims, interval=100, repeat_delay=3000, blit=True)
+    plt.show()
+
+plt.figure()
+plt.plot(E/v0, transmission)
 plt.show()
-

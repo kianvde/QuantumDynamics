e6fc5f5d2b5ddfc4f53dddf9fec87be840fa1886
LeoHoogerbrugge <hoogerbrugge.leo@gmail.com>
2015-05-08 12:30:45 +0200

    Minor edits

--- 2DCrank.py
+++ 2DCrank.py
@@ -3,6 +3,7 @@ import scipy.sparse.linalg as linalg
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import rc
 
 # simulation constants
 dt = 0.05
@@ -22,7 +23,7 @@ xGrid , yGrid = np.meshgrid(np.linspace(0,L,n),np.linspace(0,L,n))  # plotting c
 
 psi = np.exp(-0.1*((x-L/4)**2)) * np.exp(1.j*k*x)  # gaussian front
 # psi = np.exp(-0.05*((x-L/4)**2 + (y-L/2)**2)) * np.exp(1.j*k*x)  # gaussian peak
-# psi /= np.sqrt(sum(abs(psi)**2*dx**2))  # normalize the wave function
+psi /= np.sqrt(sum(abs(psi)**2*dx**2))  # normalize the wave function
 
 # set a barrier
 v = 12.5*(np.abs(3.*L/8.-x) < 0.56)
@@ -44,31 +45,30 @@ B = sparse.diags(1.-4.*c1-c2*v, 0) \
 # initialize figure for plotting
 plt.ion()
 fig = plt.figure()
-# fig2 = plt.figure()
-# ax2 = fig2.add_subplot(111)
 ax = fig.add_subplot(111, projection='3d')
 ax.set_zlim(0,2.*np.amax(np.abs(psi)**2))
 
 crit = 0.
 psiPlot = np.abs(psi.reshape(n,n)**2)
-Inter = psiPlot[:,np.round(n/2)]
+Inter = psiPlot[:,np.round(3.*n/4.)] # Screen set at x = 0.75*L
 for i in range(500):
 
     if i%10 == 0:
         psiPlot = np.abs(psi.reshape(n,n)**2)
         wireFrame = ax.plot_wireframe(xGrid, yGrid, psiPlot, rstride = 2, cstride = 2)
-        # ploty = ax2.plot(np.linspace(0,L,n),psiPlot[:,np.round(n/2)],'b')
         if sum(psiPlot[:,np.round(3.*n/4.)]) > crit:
             Inter = psiPlot[:,np.round(3.*n/4.)]
             crit = sum(psiPlot[:,np.round(3.*n/4.)])
-            print 'hallo'
 
         plt.pause(.001)
         ax.collections.remove(wireFrame)
-        # ax2.clear()
 
     [psi, garbage] = linalg.bicgstab(A, B*psi,x0=psi)
 
 plt.figure()
 plt.plot(np.linspace(0,L,n),Inter,'b')
+plt.title("Quantum double slit")
+plt.xlabel("x")
+rc('text', usetex=True)
+plt.ylabel("$|\psi|^2$")
 plt.show()
